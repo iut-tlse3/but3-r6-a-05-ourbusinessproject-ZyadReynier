ff036a905ccb1d6dfc3f2292f5bf94e8ca67c4d7
Salairedesalledebain <zyad.iscles@gmail.com>
2026-01-27 08:50:36 +0100

    initial commit spring boot project

---
+++ src/main/java/ourbusinnessproject/OurBusinnessProjectApplication.java
@@ -0,0 +1,13 @@
+package ourbusinnessproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurBusinnessProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurBusinnessProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ourbusinnessproject/OurBusinnessProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ourbusinnessproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurBusinnessProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

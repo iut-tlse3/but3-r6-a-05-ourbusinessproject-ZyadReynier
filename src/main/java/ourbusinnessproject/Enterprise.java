e99feac1e03e28544ed1461723ce2fa53e4bc0cc
Salairedesalledebain <zyad.iscles@gmail.com>
2026-01-27 11:34:43 +0100

    fix #1.2 classe Enterprise

--- src/main/java/ourbusinnessproject/Enterprise.java
+++ src/main/java/ourbusinnessproject/Enterprise.java
@@ -20,19 +20,67 @@ public class Enterprise {
 
     }
 
+    /**
+     * Set the name of the enterprise
+     * @param name String that define the name
+     */
     public void setName(String name) {
         Name = name;
     }
 
+    /**
+     * Set the description of the enterprise
+     * @param description String that describes the enterprise
+     */
     public void setDescription(String description) {
         Description = description;
     }
 
+    /**
+     * Set the Email of the contact in the enterprise
+     * @param contactEmail String that define the mail
+     */
     public void setContactEmail(String contactEmail) {
         ContactEmail = contactEmail;
     }
 
+    /**
+     * Name of the contact in the enterprise
+     * @param contactName first and last name of the contact
+     */
     public void setContactName(String contactName) {
         ContactName = contactName;
+    }
+
+    /**
+     *
+     * @return mail of the contact
+     */
+    public String getContactEmail() {
+        return ContactEmail;
+    }
+
+    /**
+     *
+     * @return first and last name of the contact
+     */
+    public String getContactName() {
+        return ContactName;
+    }
+
+    /**
+     * *
+     * @return description of the enterprise
+     */
+    public String getDescription() {
+        return Description;
+    }
+
+    /**
+     *
+     * @return name of the enterprise
+     */
+    public String getName() {
+        return Name;
     }
 }
--- src/main/java/ourbusinnessproject/Project.java
+++ src/main/java/ourbusinnessproject/Project.java
@@ -11,11 +11,35 @@ public class Project {
     public Project(){
     }
 
+    /**
+     * Set the title of the project
+     * @param title String that define the title
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Set the description of the project
+     * @param description String that describes the project
+     */
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    /**
+     *
+     * @return description of the project
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     *
+     * @return title of the project
+     */
+    public String getTitle() {
+        return title;
     }
 }

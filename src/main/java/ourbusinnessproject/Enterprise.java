ff969fd53eacd984ce8f4301c24248b44af351e4
Salairedesalledebain <zyad.iscles@gmail.com>
2026-01-27 11:38:39 +0100

    fix #1.3 Mapping Project et Enterprise

--- src/main/java/ourbusinnessproject/Enterprise.java
+++ src/main/java/ourbusinnessproject/Enterprise.java
@@ -1,8 +1,17 @@
 package ourbusinnessproject;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.*;
 
+@Entity
 public class Enterprise {
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private Integer id;
+
     @NotBlank
     private String Name;
     @Size(min = 10)
--- src/main/java/ourbusinnessproject/Project.java
+++ src/main/java/ourbusinnessproject/Project.java
@@ -1,9 +1,17 @@
 package ourbusinnessproject;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 
-
+@Entity
 public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
     @NotBlank
     private String title;
     private  String description;
